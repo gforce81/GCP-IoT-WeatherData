500265acff0f905a0e0db89df3a43795ac44dffa
Gauthier Robe <g@gforce.io>
2019-01-15 08:40:27 -0500

    sensor info cannot be a string

--- checkWeather-modified.py
+++ checkWeather-modified.py
@@ -84,7 +84,9 @@ def main():
     
     # constants - change to fit your project and location
     SEND_INTERVAL = config['CONSTANTS']['SEND_INTERVAL']#seconds
-    sensor = config['CONSTANTS']['sensor']
+    # sensor = config['CONSTANTS']['sensor']
+    sendor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+    print "Sensor Info: " + sensor
 
     ## change project to your Project ID
     project=config['PROJECT']['project']
